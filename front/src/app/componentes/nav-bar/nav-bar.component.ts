7164f268e42e9e2dbddeb8feaea6b467b87a1127
Sebastian <solaran@fi.uba.ar>
2024-02-18 17:10:21 -0300

    Comienzo de diseño UX

--- front/src/app/componentes/nav-bar/nav-bar.component.ts
+++ front/src/app/componentes/nav-bar/nav-bar.component.ts
@@ -3,19 +3,27 @@ import {Producto} from "../../interfaces/producto";
 import {RouterLink} from "@angular/router";
 
 @Component({
-  selector: 'app-nav-bar',
-  standalone: true,
-  imports: [
-    RouterLink
-  ],
-  templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+   selector: 'app-nav-bar',
+   standalone: true,
+   imports: [
+      RouterLink
+   ],
+   templateUrl: './nav-bar.component.html',
+   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-  handleButtonClick(){
-    const sidebar = document.querySelector('.sidebar')
-    // @ts-ignore
-    sidebar.classList.toggle('active')
-  }
+   handleButtonClick() {
+      const sidebar = document.querySelector('.sidebar')
+      // @ts-ignore
+      sidebar.classList.toggle('active')
+   }
 
+   toggleMenu() {
+      const menu = document.querySelector(".menu-links");
+      const icon = document.querySelector(".hamburger-icon");
+      if (menu && icon) {
+         menu.classList.toggle("open");
+         icon.classList.toggle("open");
+      }
+   }
 }
