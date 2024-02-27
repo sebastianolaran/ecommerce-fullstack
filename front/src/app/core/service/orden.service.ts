c627920d43f165442081689f9032c6331822215d
Sebastian <solaran@fi.uba.ar>
2024-02-26 21:16:22 -0300

    Cambios en la vista de nabvar

--- front/src/app/core/service/orden.service.ts
+++ front/src/app/core/service/orden.service.ts
@@ -23,21 +23,24 @@ export class OrdenService {
             'Authorization': `Bearer ${token}`
          })
       };
-      return this.http.get<Orden[]>(`${this.apiUrl}/`,httpOptions)
+      return this.http.get<Orden[]>(`${this.apiUrl}/`, httpOptions)
    }
 
    eliminarOrden(id_orden: string): Observable<any> {
+      const token = localStorage.getItem('token');
+      const httpOptions = {
+         headers: new HttpHeaders({
+            'Authorization': `Bearer ${token}`
+         })
+      };
       const credencial = {id_orden}
-      return this.http.post<any>(`${this.apiUrl}/eliminar`, credencial)
+      return this.http.post<any>(`${this.apiUrl}/eliminar`, credencial, httpOptions)
    }
 
 
    obtenerProductos(id_orden: string) {
       const token = localStorage.getItem('token');
 
-      console.log(token)
-
-      // Configurar el encabezado con el token JWT
       const httpOptions = {
          headers: new HttpHeaders({
             'Authorization': `Bearer ${token}`
@@ -45,27 +48,49 @@ export class OrdenService {
       };
       const credencial = {id_orden}
       console.log(credencial);
-      return this.http.post<any>(`${this.apiUrl}/productos`, credencial,httpOptions)
+      return this.http.post<any>(`${this.apiUrl}/productos`, credencial, httpOptions)
    }
 
 
    obtenerNuevoid(): Observable<string> {
-      return this.http.get<any>(`${this.apiUrl}/id`).pipe(
+      const token = localStorage.getItem('token');
+
+      const httpOptions = {
+         headers: new HttpHeaders({
+            'Authorization': `Bearer ${token}`
+         })
+      };
+
+      return this.http.get<any>(`${this.apiUrl}/id`,httpOptions).pipe(
          map(response => response.id_orden)
       );
    }
 
    agregarProductoConOrden(id_orden: string, id_producto: number, cantidad: number, precio_unitario: number) {
+      const token = localStorage.getItem('token');
+
+      const httpOptions = {
+         headers: new HttpHeaders({
+            'Authorization': `Bearer ${token}`
+         })
+      };
       const credencial = {id_orden, id_producto, cantidad, precio_unitario}
 
-      return this.http.post<any>(`${this.apiUrl}/producto/agregar`, credencial)
+      return this.http.post<any>(`${this.apiUrl}/producto/agregar`, credencial,httpOptions)
 
    }
 
    agregarOrden(idOrden: string, valorTotal: number, estado: string) {
+      const token = localStorage.getItem('token');
+
+      const httpOptions = {
+         headers: new HttpHeaders({
+            'Authorization': `Bearer ${token}`
+         })
+      };
       const credencial = {idOrden, valorTotal, estado}
       console.log(credencial);
-      return this.http.post<any>(`${this.apiUrl}/guardar`, credencial)
+      return this.http.post<any>(`${this.apiUrl}/guardar`, credencial,httpOptions)
 
 
    }
